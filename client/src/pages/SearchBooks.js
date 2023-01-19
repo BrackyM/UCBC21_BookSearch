689d771133f59cce213b88f752926916cb24227e
BraxtonM <braxtoncoding@gmail.com>
2023-01-18 19:13:35 -0500

    worked on the pages folder and got rid of old API endpoints that were present in the starter code

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -2,7 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
+
+import { SAVE_BOOK } from '../utils/mutations';
+import { useMutation } from '@apollo/client';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
@@ -52,6 +55,8 @@ const SearchBooks = () => {
     }
   };
 
+  const [saveBook_mutation, {error}] = useMutation(SAVE_BOOK)
+
   // create function to handle saving a book to our database
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
@@ -64,12 +69,14 @@ const SearchBooks = () => {
       return false;
     }
 
-    try {
-      const response = await saveBook(bookToSave, token);
+    // Had to delete old API endpoints
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+    try {
+
+      const {data} = await saveBook_mutation({variables: {
+        bookInfo: {...bookToSave}
+      }});
+
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
