eda02ae1183d785b47c52ff8eb6494d734ec8744
BraxtonM <braxtoncoding@gmail.com>
2023-01-18 19:36:41 -0500

    replacing server.js route options with apollo server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+
+const {ApolloServer} = require("apollo-server-express");
+
+// Authentication middleware
+const { authMiddleware } = require('./utils/auth')
+
+const { typeDefs, resolvers} = require('./schemas');
+const { isContext } = require('vm');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,8 +21,26 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// No routes will be used with SPA app.use(routes);
 
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client"));
+});
+
+const apolloServerStart = async (typeDefs, resolvers) => {
+  await server.start();
+
+  server.applyMiddleware({ app });
+}
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+apolloServerStart(typeDefs, resolvers);
